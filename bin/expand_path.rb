9bb29c2c030bd43f476a92f88810e4b6c1be4ec1
Jon Higgs <jhiggs0@gmail.com>
2013-01-28 15:00:31 +0100

    use expand_path because it is supported by ruby1.8.

--- bin/expand_path.rb
+++ bin/expand_path.rb
@@ -3,4 +3,4 @@ file = ARGV[0]
 raise "you must specfify a file" if file.nil?
 raise "file '#{file}' does not exist" unless File.exist? file
 
-puts File.absolute_path file
+puts File.expand_path file
